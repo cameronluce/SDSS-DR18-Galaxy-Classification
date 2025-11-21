5cab8708307b3e4158d1fb8dd5c1b114679aaecf
Cameron Luce <up2121518@myport.ac.uk>
2025-11-21 13:36:02 +0000

    attempting to fix calling of functions from py file

--- py/functions.py
+++ py/functions.py
@@ -2,9 +2,6 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import kagglehub
-from kagglehub import KaggleDatasetAdapter
-
 
 #all helper functions used
 url = "https://www.kaggle.com/api/v1/datasets/download/bryancimo/sdss-galaxy-classification-dr18"
