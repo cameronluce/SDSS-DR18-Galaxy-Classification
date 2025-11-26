47ff68c319c875bc9d450091d2a869721072fd86
Cameron Luce <up2121518@myport.ac.uk>
2025-11-26 22:17:32 +0000

    created README and fixed calling functions

--- py/functions.py
+++ py/functions.py
@@ -1,11 +1,9 @@
-#imports
-import kagglehub
-from kagglehub import KaggleDatasetAdapter
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
+import kagglehub #used to get the data from kaggle.com
+from kagglehub import KaggleDatasetAdapter #used to fetch the specific dataset from kaggle
+import pandas as pd #used for viewing and manipulating the data
+import matplotlib.pyplot as plt #used for data visulisation
 
 #all helper functions used below
-def data_grabber():
-    df = kagglehub.dataset_load(KaggleDatasetAdapter.PANDAS, "bryancimo/sdss-galaxy-classification-dr18", "sdss_100k_galaxy_form_burst.csv")
+def data_grabber(): #fetches data from kaggle.com and returns it
+    df = kagglehub.dataset_load(KaggleDatasetAdapter.PANDAS, "bryancimo/sdss-galaxy-classification-dr18", "sdss_100k_galaxy_form_burst.csv", pandas_kwargs={'skiprows': 1})
     return df
