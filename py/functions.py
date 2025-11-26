0fb3f5c0249f0e1cd326f05223823c95846762de
Cameron Luce <up2121518@myport.ac.uk>
2025-11-26 13:12:42 +0000

    made changes to q1_notebook and functions

--- py/functions.py
+++ py/functions.py
@@ -3,9 +3,9 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-#all helper functions used
+#all helper functions used below
 url = "https://www.kaggle.com/api/v1/datasets/download/bryancimo/sdss-galaxy-classification-dr18"
 
 def data_grabber():
-    data = pd.read_csv(url)
+    data = pd.read_csv(url, encoding='ISO-8859-1')
     return data
