1854292eb3f6b3adb502233862151716dd5eed50
Cameron Luce <up2121518@myport.ac.uk>
2025-11-26 16:07:17 +0000

    stopped working on functions to focus more on data

--- py/functions.py
+++ py/functions.py
@@ -1,11 +1,11 @@
 #imports
+import kagglehub
+from kagglehub import KaggleDatasetAdapter
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
 #all helper functions used below
-url = "https://www.kaggle.com/api/v1/datasets/download/bryancimo/sdss-galaxy-classification-dr18"
-
 def data_grabber():
-    data = pd.read_csv(url, encoding='ISO-8859-1')
-    return data
+    df = kagglehub.dataset_load(KaggleDatasetAdapter.PANDAS, "bryancimo/sdss-galaxy-classification-dr18", "sdss_100k_galaxy_form_burst.csv")
+    return df
